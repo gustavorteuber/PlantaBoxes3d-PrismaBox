03a86c993250ffe9271fbe43cc9a2b841575cf09
gustavorteuber <gugareis135@gmail.com>
2023-05-19 14:40:05 -0300

    aprox-distance

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,8 @@
 <script>
 export default {
   mounted() {
-    // Cria a cena
     this.scene = new THREE.Scene();
 
-    // Cria a câmera
     this.camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
@@ -17,23 +15,19 @@ export default {
     );
     this.camera.position.z = 10;
 
-    // Cria o renderizador
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // Adiciona o renderizador ao elemento do Vue
     this.$refs.container.appendChild(this.renderer.domElement);
 
-    // Cria os eixos
     this.createAxes();
 
-    // Cria o chão quadriculado
     this.createGrid();
 
-    // Adiciona eventos de interação do mouse
     this.addMouseListeners();
 
-    // Renderiza a cena
+    this.addMouseWheelListener();
+
     this.animate();
   },
   methods: {
@@ -45,10 +39,8 @@ export default {
       const gridSize = 40;
       const divisions = 40;
 
-      // Cria o material do chão quadriculado
       const gridMaterial = new THREE.LineBasicMaterial({ color: 0x888888 });
 
-      // Cria o chão quadriculado
       const gridHelper = new THREE.GridHelper(
         gridSize,
         divisions,
@@ -58,12 +50,10 @@ export default {
       this.scene.add(gridHelper);
     },
     addMouseListeners() {
-      // Captura a posição inicial do mouse
       this.mouse = new THREE.Vector2();
       this.previousMouse = new THREE.Vector2();
       this.isMouseDown = false;
 
-      // Adiciona os eventos de interação do mouse
       document.addEventListener("mousedown", this.onMouseDown);
       document.addEventListener("mousemove", this.onMouseMove);
       document.addEventListener("mouseup", this.onMouseUp);
@@ -71,22 +61,15 @@ export default {
     onMouseDown(event) {
       event.preventDefault();
       this.isMouseDown = true;
-
-      // Atualiza a posição inicial do mouse
       this.previousMouse.set(event.clientX, event.clientY);
     },
     onMouseMove(event) {
       event.preventDefault();
       if (this.isMouseDown) {
-        // Calcula o movimento do mouse
         this.mouse.set(event.clientX, event.clientY);
         const delta = this.mouse.clone().sub(this.previousMouse);
-
-        // Atualiza a rotação da cena com base no movimento do mouse
         this.scene.rotation.x += delta.y * 0.01;
         this.scene.rotation.y += delta.x * 0.01;
-
-        // Atualiza a posição anterior do mouse
         this.previousMouse.set(event.clientX, event.clientY);
       }
     },
@@ -94,10 +77,20 @@ export default {
       event.preventDefault();
       this.isMouseDown = false;
     },
+    addMouseWheelListener() {
+      this.$refs.container.addEventListener("wheel", this.onMouseWheel);
+    },
+    onMouseWheel(event) {
+      event.preventDefault();
+      const delta = event.deltaY * 0.1;
+      this.camera.position.z += delta;
+      this.camera.position.z = Math.max(
+        2,
+        Math.min(50, this.camera.position.z)
+      );
+    },
     animate() {
       requestAnimationFrame(this.animate);
-
-      // Renderiza a cena com a câmera
       this.renderer.render(this.scene, this.camera);
     },
   },
